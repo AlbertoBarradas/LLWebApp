72c9464778e12a8ffe80faf7209b021d46f1bd93
AlbertoBarradas <sebastianbago@hotmail.com>
2025-09-14 17:35:58 -0600

    README updated

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LLWebApp
 Development of an API that handles table bookings for a ficticious restaurant
+/restaurant/
+/restaurant/menu
+/restaurant/booking/tables/
+/restaurant/users
--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon',
         'USER': 'root',
-        'PASSWORD': 'tarantulino',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
